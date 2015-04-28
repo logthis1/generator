18a34719dec3bd3653612c745828dd774add77dc
logthis1 <him@stevenmathews.com>
2015-04-28 13:01:38 +0100

    Create square.py

---
+++ square.py
@@ -0,0 +1,24 @@
+import math
+import decimal
+import matplotlib.pyplot as plt
+
+def drange(start, stop, step):
+    while start <= stop:
+        yield start
+        start += step
+
+def squarewave(freq, amp):    
+    squarev = []
+    a = amp
+    f = freq
+    phase = (2*math.pi*f)/4096
+    for i in drange(0, 1, 0.000244140625):
+        if phase < math.pi:
+            squarev.append(a)
+        else:
+            squarev.append(-a)
+        phase = phase + ((2*math.pi*f)/4096)
+        if phase > (2*math.pi):
+            phase = phase - (2*math.pi)
+    plt.plot(squarev)
+    plt.show()
